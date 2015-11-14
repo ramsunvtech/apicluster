76b4975c0773086721be6ff7c3a1ec08fd93b8b8
Venkatraman <ramsunvtech@gmail.com>
2015-11-14 22:23:45 +0530

    Added Spec Test Case

---
+++ spec/api-cluster-conf.js
@@ -0,0 +1,5 @@
+describe('In defaults() / addAnother()', function () {
+	it('is has Version', function () {
+		expect(true).toBe(true);
+	})
+});
---
+++ spec/general.js
@@ -0,0 +1,5 @@
+describe('General', function () {
+	it('has Version of 0.0.1', function () {
+		expect(ApiCluster.VERSION).toBe('0.0.1');
+	})
+});
