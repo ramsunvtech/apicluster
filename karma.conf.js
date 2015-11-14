9f011cb59fb141bf128a10a00f48fd28723bcda6
Venkatraman <ramsunvtech@gmail.com>
2015-11-14 21:33:01 +0530

    Karma Configuration

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'spec/hello.js'
+      'ApiCluster.js',
+      'spec/general.js',
     ],
 
 
