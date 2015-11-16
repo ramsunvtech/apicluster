5dd8b368c5452d50f9778bb31ef53cf2e5fc230e
Venkatraman <ramsunvtech@gmail.com>
2015-11-16 21:28:08 +0530

    Renamed from spec to test

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'ApiCluster.js',
-      'spec/general.js',
+      'test/general.js',
     ],
 
 
